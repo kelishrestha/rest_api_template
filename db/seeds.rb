d34afeeb031cffa79231f87916ecd0adbd78899a
kelishrestha <kylesth@gmail.com>
2017-01-28 17:31:09 +0545

    Create api key with default token.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Create Api Key
+ApiKey.find_or_create_by!(token: 'apptoken')
